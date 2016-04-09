b181bb4f0fbefd7df67fefff8fd877b284edfa05
Lynnsay <lynnsaymay.piper@outlook.com>
2016-04-09 17:51:02 +0100

    Added Test Stubs to fail for GetPrimes

--- PrimeMultiSq.Calculations.UnitTests/PrimeTests.cs
+++ PrimeMultiSq.Calculations.UnitTests/PrimeTests.cs
@@ -7,9 +7,53 @@ namespace PrimeMultiSq.Calculations.UnitTests
     public class PrimeTests
     {
         [TestMethod]
-        public void GetPrime_ParamNumberOfPrimesZero_Expected_ArgumentNullException()
+        public void GetPrimes_ParamNumberOfPrimesZero_Expected_ArgumentNullException()
         {
             Assert.Fail();
         }
+
+        [TestMethod]
+        public void GetPrimes_ParamNumberOfPrimesOne_Expected_ReturnPrimeCountOne()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetPrimes_ParamNumberOfPrimesOne_Expected_ReturnedPrimeNumber_Two()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetPrimes_ParamNumberOfPrimesTwo_Expcted_ReturnPrimeCountTwo()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetPrimes_ParamNumberOfPrimesTwo_Expected_ReturnPrimeNumbers_TwoAndThree()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetPrimes_ParamNumberOfPrimesRandom_Expected_ReturnedCorrectNumber()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetPrimes_ParamNumberOfPrimesRandomSeedTen_Expected_ReturnedCorrectNumber()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void GetPrimes_ParamNumberOfPrimesRandomSeedOneHundred_Expected_ReturnedCorrectNumber()
+        {
+            Assert.Fail();
+        }
+
+
     }
 }
