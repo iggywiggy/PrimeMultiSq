7266ba8fc8266fdc4f0859b65037671f497dbe92
Lynnsay <Lynnsay@DESKTOP-TICSSFM>
2016-04-10 19:03:36 +0100

    Created Grid output interface and test class

---
+++ PrimeMultiSq.BusinessLogic.Interfaces/IGridOutput.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeMultiSq.BusinessLogic.Interfaces
+{
+    public interface IGridOutput
+    {
+        StringBuilder FormatOutput(IEnumerable<int[]> grid);
+    }
+}
---
+++ PrimeMultiSq.BusinessLogic.UnitTests/GridOutputTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrimeMultiSq.BusinessLogic.Interfaces;
+using Assert = NUnit.Framework.Assert;
+
+namespace PrimeMultiSq.BusinessLogic.UnitTests
+{
+    [TestClass]
+    public class GridOutputTests
+    {
+        private IGridOutput _gridOutput;
+
+        [TestMethod]
+        public void FormatOutput_ParamGridValueNull_ThrowsArgumentNullException()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void FormatOutput_ParamGridValueZeroEntries_ThrowArgumentNullException()
+        {
+            Assert.Fail();
+        }
+    }
+}
