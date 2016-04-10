2bce0ee8d99d8bb65d491eff9ba0ffbe1013eaf7
Lynnsay <Lynnsay@DESKTOP-TICSSFM>
2016-04-10 14:10:33 +0100

    Added unit tests for multiplication row creation

---
+++ PrimeMultiSq.Calculations.Interfaces/IRowCreator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeMultiSq.Calculations.Interfaces
+{
+    public interface IRowCreator
+    {
+        int[] CreateRow(int primeLeft, int[] primeTopRow);
+    }
+}
---
+++ PrimeMultiSq.Calculations.UnitTests/RowCreatorTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assert = NUnit.Framework.Assert;
+using NUnit.Framework;
+
+namespace PrimeMultiSq.Calculations.UnitTests
+{
+    [TestClass]
+    public class RowCreatorTests
+    {
+
+        [TestMethod]
+        public void CreateRow_ParamPrimeLeftIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void CreateRow_ParamPrimeLeftIsNotNull_ParamPrimeTopRowIsNull_ThrowsArgumentNullException()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void CreateRow_ParamPrimeLeftIsNotNull_ParamPrimeTopRowCountIs0_ThrowsArgumentNullException()
+        {
+            Assert.Fail();
+        }
+
+        [TestCase(2, new[] { 2 }, ExpectedResult = 1)]
+        [TestCase(2, new[] { 2, 3, 5, 7, 11}, ExpectedResult = 5)]
+        [TestCase(2, new[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 }, ExpectedResult = 10)]
+        public int[] CreateRow_ReturnCountIsCorrect(int primeLeft, int[] topRowPrimes)
+        {
+            return null;
+        }
+
+        [TestCase(2, new[] { 2, 3, 5 }, ExpectedResult = new [] { 2, 6, 10 })]
+        [TestCase(3, new[] { 2, 3, 5 }, ExpectedResult = new [] { 6, 9, 15 })]
+        public int[] CreateRow_GeneratedMultiplicationsEqual(int primeLeft, int[] primeTopRow)
+        {
+            return null;
+        }
+    }
+}
